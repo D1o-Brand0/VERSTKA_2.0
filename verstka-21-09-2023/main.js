1fdc61aec0e0dcb999f613f9701f6d5d3741810a
D1o-Brand0 <80870367+D1o-Brand0@users.noreply.github.com>
2023-09-30 17:49:41 +0900

    Добавлен Swiper

--- verstka-21-09-2023/main.js
+++ verstka-21-09-2023/main.js
@@ -1,3 +1,53 @@
 import './style.css'
 import './assets/fonts/stylesheet.css'
 import './blocks/common.sass'
+
+
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Navigation arrows
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        360: { 
+            slidesPerView: 1,
+            pagination:{
+                el: ".swiper-pagination",
+                type: "fraction",
+            },
+        },
+        768: { 
+            slidesPerView: 3,
+            spaceBetween: 24,
+            pagination:{
+                el: ".swiper-pagination",
+                type: 'bullets',
+                clickable: true,
+                fractionDigits: 2
+            },
+        },
+        1352: { 
+            slidesPerView: 4,
+            spaceBetween: 24,
+            pagination:{
+                el: ".swiper-pagination",
+            },
+        }
+    } 
+})
+
+
